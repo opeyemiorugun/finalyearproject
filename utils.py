aa278891c08bedaa5ffdedd7724fe729d9336e2c
Opeyemi Orugun <105828431+opeyemiorugun@users.noreply.github.com>
2024-06-20 21:55:59 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -18,6 +18,7 @@ def fetch_github_file(url):
         st.error(f"Failed to fetch file from {url}")
         return None
 
+@st.cache_data
 def load_model(appliances):
     base_url = "https://raw.githubusercontent.com/opeyemiorugun/finalyearproject/master/models/"  # Update with your GitHub repository URL
     models = {}
@@ -33,147 +34,14 @@ def load_model(appliances):
             st.error(f"Model file for {appliance} not found.")
     return models
 
-@st.cache_data
-def preprocess_data(data, column_names):
-    def handle_missing_values(df):
-        resampled_df = df.resample('6s').mean()
-        for col in resampled_df.columns:
-            resampled_df[col] = pd.to_numeric(resampled_df[col], errors='coerce')
-        final_df = resampled_df.interpolate(method='linear')
-        na_columns = final_df.columns[final_df.isna().any()].tolist()
-        for col in na_columns:
-            final_df[col] = final_df[col].fillna(0)
-        return final_df
-
-    def handle_standby_values(df, column_names):
-        final_standby = df
-        for column in column_names:
-            state_column = f'{column}_state'
-            final_standby[state_column] = final_standby[column].apply(lambda x: 'off' if x == 0 else 'unknown')
-        for column in column_names:
-            state_column = f'{column}_state'
-            non_zero_values = final_standby[final_standby[column] != 0][column].values.reshape(-1, 1)
-            if len(non_zero_values) > 1:
-                kmeans = KMeans(n_clusters=2, random_state=0, n_init=10).fit(non_zero_values)
-                standby_cluster = np.argmin(kmeans.cluster_centers_)
-                active_cluster = 1 - standby_cluster
-                cluster_mapping = {standby_cluster: 'standby', active_cluster: 'active'}
-                final_standby.loc[final_standby[column] != 0, state_column] = kmeans.labels_
-                final_standby[state_column] = final_standby[state_column].replace(cluster_mapping)
-            else:
-                if len(non_zero_values) == 1:
-                    final_standby.loc[final_standby[column] != 0, state_column] = 'active'
-        standby_active = {"kettle": 2000, "fridge_freezer": 50, "dishwasher": 10, "microwave": 200, "washer_dryer": 20}
-        for key in standby_active.keys():
-            if key in column_names:
-                final_standby[f"{key}_state"] = np.where(final_standby[key] > standby_active[key], "active", final_standby[f"{key}_state"])
-        return final_standby
-
-    def handle_outliers(df, column_names, lower_quantile=0.01, upper_quantile=0.99):
-        final_df_c = df.copy()
-        final_df_c["timestamp"] = df.index
-        df_cleaned = final_df_c.copy()
-        for column in column_names[1:]:
-            if column != 'timestamp':
-                appliance_data = final_df_c[column]
-                if column != "aggregate":
-                    appliance_state = final_df_c[f"{column}_state"]
-                    active_data = appliance_data[appliance_state == 'active']
-                else:
-                    active_data = appliance_data[appliance_data > 0]
-                lower_bound = active_data.quantile(lower_quantile)
-                upper_bound = active_data.quantile(upper_quantile)
-                if column != 'aggregate':
-                    cleaned_data = appliance_data[(appliance_state != 'active') | 
-                                            ((appliance_data >= lower_bound) & (appliance_data <= upper_bound))]
-                else:
-                    cleaned_data = appliance_data[(appliance_data == 0) | 
-                                                ((appliance_data >= lower_bound) & (appliance_data <= upper_bound))]
-                df_cleaned[column] = cleaned_data   
-        df_cleaned.dropna(inplace=True)
-        df_p = final_df_c.loc[df_cleaned.index]
-        return df_p
-
-    def feature_engineering(df, column_names):
-        df_p = df.copy()
-        new_columns = {}
-        for col in column_names:
-            standby = df_p.loc[df_p[f'{col}_state'] == 'standby', col]
-            avg_standby = standby.mean()
-            new_columns[f'{col}_avg_standby'] = [avg_standby] * len(df_p)
-            total_power_consumption = df_p[col].sum()
-            total_standby_consumption = standby.sum()
-            standby_ratio = total_standby_consumption / total_power_consumption if total_power_consumption != 0 else 0
-            new_columns[f'{col}_standby_ratio'] = [standby_ratio] * len(df_p)
-            deviation = df_p[col] - avg_standby
-            new_columns[f'{col}_deviation'] = deviation
-        new_columns_df = pd.DataFrame(new_columns, index=df_p.index)
-        df_p = pd.concat([df_p, new_columns_df], axis=1)
-        df_p = df_p.copy()
-        df_p = pd.get_dummies(df_p, drop_first=False)
-        temp_hum = st.session_state["weather_data"]
-        temp_hum['datetime'] = pd.to_datetime(temp_hum.YEAR.astype(str) + '-' + temp_hum.MO.astype(str) +'-'
-                                            + temp_hum.DY.astype(str) + ' ' + temp_hum.HR.astype(str) + ":00:00" )
-        temp_hum.set_index('datetime', inplace=True)
-        temp_hum.drop(['YEAR', 'MO', 'DY', 'HR'], axis=1, inplace=True)
-        weather = temp_hum.resample('6s').interpolate()
-        data = df_p.join(weather)
-        data['datetime'] = data.index
-        data['hour'] = data['datetime'].dt.hour
-        data['dayofweek'] = data['datetime'].dt.dayofweek
-        data['quarter'] = data['datetime'].dt.quarter
-        data['year'] = data['datetime'].dt.year
-        data['dayofyear'] = data['datetime'].dt.dayofyear
-        data['dayofmonth'] = data['datetime'].dt.day
-        data['month'] = data['datetime'].dt.month
-        data.drop(["datetime"], axis=1, inplace=True)
-        lagged_features = []
-        for col in column_names:
-            if col not in ['dayofweek','datetime', 'T2M', 'RH2M', 'month', 'year', 'hour', 'quarter', 'dayofyear', 'dayofmonth']:
-                lagged_features.append(df[col].shift(1).rename(f'{col}_prev_day'))
-        lagged_df = pd.concat([data] + lagged_features, axis=1)
-        fourier = CalendarFourier(freq="YE", order=10)
-        dp = DeterministicProcess(
-            index=data.index,
-            constant=True,
-            order=1,
-            seasonal=True,
-            period=7,
-            additional_terms=[fourier],
-            drop=True,
-        )
-        det_d = dp.in_sample()
-        print(det_d.columns)
-        combined = pd.merge(lagged_df, det_d, left_index=True, right_index=True)
-        output = {"main": combined, "extra": det_d}
-        return output
-
-    df = data
-    data = handle_missing_values(df)
-    data = handle_standby_values(data, column_names)
-    data = handle_outliers(data, column_names)
-    data = feature_engineering(data, column_names)
-    return data
-
 def train_split_test(data, app):
-    # combined = data["main"]
-    # det_d = data["extra"]
-    # time_features = ['dayofweek', 'T2M', 'RH2M', 'month', 'year', 'hour', 'quarter', 'dayofyear', 'dayofmonth']
-    # det_features = [col for col in det_d.columns]
-    # appliance_cols = [col for col in combined.columns if app in col]
-    # keep_cols = time_features + det_features + appliance_cols
-    # appliance_df = combined[keep_cols]
-    # test_df = appliance_df.loc["2014-10-01 12:00:00":]
     X_test, y_test = data.drop(columns=[app]), data[app]
     test = {"X_test": X_test, "y_test": y_test}
     return test
 
 def forecast_power(data, selected_appliances, models, horizon):
-    # Preprocess the historical data first
-    combined = data["main"]
     # Generate future dates for the forecast period
-    future_dates = pd.date_range(start=combined.index[-1] + pd.Timedelta(seconds=6), periods=horizon, freq='6S')
-    
+    future_dates = pd.date_range(start=combined.index[-1] + pd.Timedelta(seconds=6), periods=horizon, freq='6s')
     forecast_df = pd.DataFrame(index=future_dates)
 
     # Loop through each selected appliance to generate forecasts
@@ -181,10 +49,10 @@ def forecast_power(data, selected_appliances, models, horizon):
         model = models[appliance]
         
         # Create an empty DataFrame for future dates with the same columns as pre_processed_data
-        future_df = pd.DataFrame(index=future_dates, columns=combined.columns)
+        future_df = pd.DataFrame(index=future_dates, columns=data.columns)
         
         # Concatenate historical data with future DataFrame
-        full_df = pd.concat([combined, future_df])
+        full_df = pd.concat([data, future_df])
         
         # Handle empty rows in the concatenated DataFrame
         full_df = full_df.fillna(method='ffill').fillna(method='bfill')
